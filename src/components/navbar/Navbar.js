95078d26b94bafe6d3a0807f2c7382a11f548a21
FITSCH YANN <Fitsch-yann@hotmail.fr>
2023-03-27 10:33:00 +0200

    burger animation

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,14 +4,24 @@ import logo from "../../assets/oiseau.png";
 
 function Navbar() {
   const [openMenu, setOpenMenu] = useState(false);
+  const [bar1, setBar1] = useState("bar1");
+  const [bar2, setBar2] = useState("bar2");
+  const [bar3, setBar3] = useState("bar3");
 
   function menu() {
     if (!openMenu) {
       setOpenMenu(true);
-    } else setOpenMenu(false);
+      setBar1("bar1-open");
+      setBar2("bar2-open");
+      setBar3("bar3-open");
+    } else {
+      setOpenMenu(false);
+      setBar1("bar1");
+      setBar2("bar2");
+      setBar3("bar3");
+    }
   }
 
-  console.log(openMenu);
   return (
     <div className="global-navbar">
       <div className="navbar">
@@ -25,18 +35,60 @@ function Navbar() {
       </div>
       <div className="menu">
         <div className="button" onClick={menu}>
-          <div className="bar1"></div>
-          <div className="bar2"></div>
-          <div className="bar3"></div>{" "}
+          <div className={bar1}></div>
+          <div className={bar2}></div>
+          <div className={bar3}></div>
         </div>
-        {openMenu ? null : (
+        {!openMenu ? null : (
           <div className="menuOpen">
-            <a href="#Accueil">Accueil</a>
-            <a href="#QuiSuisJe">Qui suis-je</a>
-            <a href="#Hypnose">Hypnose</a>
-            <a href="#Boutique">Boutique</a>
-            <a href="#Faq">Faq</a>
-            <a href="#Contact">Contact</a>
+            <a
+              href="#Accueil"
+              onClick={() => {
+                setOpenMenu(false);
+              }}
+            >
+              Accueil
+            </a>
+            <a
+              href="#QuiSuisJe"
+              onClick={() => {
+                setOpenMenu(false);
+              }}
+            >
+              Qui suis-je
+            </a>
+            <a
+              href="#Hypnose"
+              onClick={() => {
+                setOpenMenu(false);
+              }}
+            >
+              Hypnose
+            </a>
+            <a
+              href="#Boutique"
+              onClick={() => {
+                setOpenMenu(false);
+              }}
+            >
+              Boutique
+            </a>
+            <a
+              href="#Faq"
+              onClick={() => {
+                setOpenMenu(false);
+              }}
+            >
+              Faq
+            </a>
+            <a
+              href="#Contact"
+              onClick={() => {
+                setOpenMenu(false);
+              }}
+            >
+              Contact
+            </a>
           </div>
         )}
       </div>

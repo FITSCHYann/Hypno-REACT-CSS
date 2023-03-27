0092b0cee94c02988316a7486cb90c83516a641b
FITSCH YANN <Fitsch-yann@hotmail.fr>
2023-03-27 12:13:18 +0200

    test text

--- src/components/Accueil/Accueil.js
+++ src/components/Accueil/Accueil.js
@@ -6,9 +6,18 @@ function Accueil() {
     <div id="Accueil" className="global-Accueil">
       <div className="Accueil">
         <div className="text-acceuil">
-          <div>Et si le plus beau projet</div>
-          <div>sur lequel vous pouviez travailler...</div>
-          <div>c'était vous ?</div>
+          <p>Et si le plus beau projet</p>
+          <p>sur lequel vous pouviez travailler...</p>
+          <p>c'était vous ?</p>
+          <br />
+          <br />
+          <br />
+          <p>
+            Installée à Hangenbieten,
+            <br />
+            je pratique l’Hypnose et vous accueille dans mon cabinet pour vous
+            accompagner à créer votre changement !
+          </p>
         </div>
         <a href="#Contact">Contactez-moi</a>
       </div>
